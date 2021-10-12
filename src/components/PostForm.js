d28a9e87bd9008c81bad899e43232d290fade283
indianic <indianic@ind998.local>
2021-10-12 10:12:05 +0530

    third commit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,56 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Button, TextField, Container } from "@mui/material";
-import post from "../assets/images/post.gif";
+import postImg from "../assets/images/postImg.gif";
 import "./PostForm.css";
 
-const handleSubmit = () => {};
-
 const PostForm = () => {
-  return (
-    <Container maxWidth="sm">
-      <Grid container direction="row" justify="center" className="" xs={12}>
-        <Grid item sm={6}>
-          <img src={post} alt={post} />
-        </Grid>
-        <Grid item sm={6}>
-          <form
-            autoComplete="off"
-            className="post_form"
-            noValidate
-            onSubmit={handleSubmit}
-          >
-            <TextField
-              className="form_textfield"
-              name="title"
-              variant="outlined"
-              label="Title"
-              fullWidth
-            />
-            <TextField
-              name="message"
-              className="form_textfield"
-              variant="outlined"
-              label="Message"
-              fullWidth
-              multiline
-              rows={4}
-            />
+  const [post, setPost] = useState({ title: "", message: "" });
 
-            <Button
-              className="button_submit"
-              variant="contained"
-              size="large"
-              type="submit"
-              fullWidth
+  const InputChange = (event) => {
+    setPost({ ...post, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Container>
+      <div className="form_card">
+        <Grid
+          container
+          direction="row"
+          justify="flex-between"
+          className=""
+          xs={12}
+        >
+          <Grid item sm={6}>
+            <img src={postImg} alt="post figure" height="300px" />
+          </Grid>
+          <Grid item sm={6}>
+            <form
+              autoComplete="off"
+              className="post_form"
+              noValidate
+              onSubmit={handleSubmit}
             >
-              Submit
-            </Button>
-            <Button variant="contained" size="large" type="reset" fullWidth>
-              Clear
-            </Button>
-          </form>
+              <TextField
+                className="form_textfield"
+                name="title"
+                variant="outlined"
+                label="Title"
+                value={post.title}
+                onChange={InputChange}
+                fullWidth
+              />
+              <TextField
+                name="message"
+                className="form_textfield"
+                variant="outlined"
+                label="Message"
+                value={post.message}
+                onChange={InputChange}
+                fullWidth
+                multiline
+                rows={4}
+              />
+
+              <Button
+                className="button_submit"
+                variant="contained"
+                size="large"
+                type="submit"
+                fullWidth
+              >
+                Submit
+              </Button>
+              <Button variant="contained" size="large" type="reset" fullWidth>
+                Clear
+              </Button>
+            </form>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </Container>
   );
 };
